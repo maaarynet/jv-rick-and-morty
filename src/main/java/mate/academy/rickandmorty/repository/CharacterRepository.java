09433ee9fa7933208363d49718561b119e521762
maaarynet <mashajazz.mj@gmail.com>
2025-08-01 20:07:36 +0300

    fixed getRandomCharachter method

--- src/main/java/mate/academy/rickandmorty/repository/CharacterRepository.java
+++ src/main/java/mate/academy/rickandmorty/repository/CharacterRepository.java
@@ -1,13 +1,17 @@
 package mate.academy.rickandmorty.repository;
 
 import java.util.List;
+import java.util.Optional;
 import mate.academy.rickandmorty.model.Character;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 
 public interface CharacterRepository extends
         JpaRepository<Character, Long>,
         JpaSpecificationExecutor<Character> {
+    @Query(value = "SELECT * FROM characters ORDER BY RAND() LIMIT 1", nativeQuery = true)
+    Optional<Character> getRandomCharacter();
 
     List<Character> findByNameContainingIgnoreCase(String name);
 }
--- src/main/java/mate/academy/rickandmorty/service/CharacterServiceImpl.java
+++ src/main/java/mate/academy/rickandmorty/service/CharacterServiceImpl.java
@@ -1,10 +1,8 @@
 package mate.academy.rickandmorty.service;
 
 import java.util.List;
-import java.util.Random;
 import lombok.RequiredArgsConstructor;
 import mate.academy.rickandmorty.exception.CharacterNotFoundException;
-import mate.academy.rickandmorty.exception.EmptyDatabaseException;
 import mate.academy.rickandmorty.model.Character;
 import mate.academy.rickandmorty.repository.CharacterRepository;
 import org.springframework.stereotype.Service;
@@ -16,14 +14,8 @@ public class CharacterServiceImpl implements CharacterService {
 
     @Override
     public Character getRandomCharacter() {
-        long recordCount = characterRepository.count();
-        if (recordCount == 0) {
-            throw new EmptyDatabaseException("No characters in database");
-        }
-        int randomId = new Random().nextInt((int) recordCount) + 1;
-        return characterRepository.findById((long) randomId)
-                .orElseThrow(() -> new CharacterNotFoundException(
-                        "Not found Character with id " + randomId));
+        return characterRepository.getRandomCharacter()
+                .orElseThrow(() -> new CharacterNotFoundException("No characters found"));
     }
 
     @Override
